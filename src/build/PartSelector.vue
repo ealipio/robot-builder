7756fa77571e3fd0eb14265fe9f01701101e5844
Eisson Alipio <eisson.alipio@gmail.com>
2019-04-01 15:57:20 -0500

    feat: passing data using router-link

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="part" :class="position">
-    <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
+    <router-link :to="{
+        name: 'Parts',
+        params: {
+          id: this.selectedPart.id,
+          partType: this.selectedPart.type,
+        }}">
+    <img :src="selectedPart.src" title="arm"/>
+    </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -41,15 +48,6 @@ export default {
     this.emitSelectedPart();
   },
   methods: {
-    showPartInfo() {
-      this.$router.push({
-        name: 'Parts',
-        params: {
-          id: this.selectedPart.id,
-          partType: this.selectedPart.type,
-        },
-      });
-    },
     emitSelectedPart() {
       this.$emit('partSelected', this.selectedPart);
     },
