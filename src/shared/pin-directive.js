883c9f0388bec228daf91bc50d1a88afa7075d3e
Eisson Alipio <eisson.alipio@gmail.com>
2019-04-06 12:11:47 -0500

    refactor: suing shorthand  for bind and update hooks

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,15 +1,6 @@
-function applyStyle(element, binding) {
+export default function (element, binding) {
   Object.keys(binding.value).forEach((position) => {
     element.style[position] = binding.value[position];
   });
   element.style.position = 'absolute';
 }
-
-export default {
-  bind: (element, binding) => {
-    applyStyle(element, binding);
-  },
-  update: (element, binding) => {
-    applyStyle(element, binding);
-  },
-};
