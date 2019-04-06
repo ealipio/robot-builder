38c2f4fe50b68eca3470986db5c1c5fc2ad9dc3f
Eisson Alipio <eisson.alipio@gmail.com>
2019-04-05 23:31:43 -0500

    feat: using args and modifiers to pass data to directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,7 +1,10 @@
 export default {
-  bind: element => {
+  bind: (element, binding) => {
+    if (binding.arg !== 'position') return;
+
+    Object.keys(binding.modifiers).forEach((key) => {
+      element.style[key] = '5px';
+    });
     element.style.position = 'absolute';
-    element.style.bottom = '5px';
-    element.style.right = '5px';
-  }
+  },
 };
